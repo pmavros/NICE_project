7b416e6f56aff55b7a970322fe165e8fd8471921
Panos <mavros.panos@gmail.com>
2022-01-24 11:55:50 +0800

    add psych scales

---
+++ OnlineExperiment/src/scales.js
@@ -0,0 +1,314 @@
+/* SCALES */
+
+/* SAM */
+/* Willingness to walk */
+/* Beauty */
+
+
+/* Locus of control IPC - Levenson */
+// ipc_scoring
+// internal = 1,4,5,9, 18, 19, 21,23
+// powerfulothers = 3,8, 11, 13, 15, 17, 20, 22
+// chance = 2,6,7,10, 12, 14,16,24
+var ipc_instruction = "";
+var ipc = [
+	"Whether or not I get to be a leader depends mostly on my ability.",
+	"To a great extent my life is controlled by accidental happenings.",
+	"I feel like what happens in my life is mostly determined by powerful people.",
+	"Whether or not I get into a car accident depends mostly on how good a driver I am.",
+	"When I make plans, I am almost certain to make them work.",
+	"Often there is no chance of protecting my personal interests from bad luck.",
+	"When I get what I want, it’s usually because I’m lucky.",
+	"Although I might have good ability, I will not be given leadership responsibility without appealing to those in positions of power.",
+	"How many friends I have depends on how nice a person I am.",
+	"I have often found that what is going to happen will happen.",
+	"My life is chiefly controlled by powerful others.",
+	"Whether or not I get into a car accident is mostly a matter of luck.",
+	"People like myself have very little chance of protecting our personal interests when they conflict with those of strong pressure groups.",
+	"It’s not always wise for me to plan too far ahead because many things turn out to be a matter of good or bad fortune.",
+	"Getting what I want requires pleasing those people above me.",
+	"Whether or not I get to be a leader depends on whether I’m lucky enough to be in the right place at the right time.",
+	"If important people were to decide they didn’t like me, I probably wouldn’t make many friends.",
+	"I can pretty much determine what will happen in my life.",
+	"I am usually able to protect my personal interests.",
+	"Whether or not I get into a car accident depends mostly on the other driver.",
+	"When I get what I want, it’s usually because I worked hard for it.",
+	"In order to have my plans work, I make sure that they fit in with the desires of people who have power over me.",
+	"My life is determined by my own actions.",
+	"It’s chiefly a matter of fate whether or not I have a few friends or many friends."
+];
+
+
+
+/* IPIP6
+
+"This part of the questionnaire is about your personality. Please select a response that best represents how accurately each statement describes you.
+1-7 point scale: from Very Inaccurate	  - Very Accurate"
+*/
+
+var ipip6_instruction = "This part of the questionnaire is about your personality. Please select a response that best represents how accurately each statement describes you.";
+
+var ipip6 = [
+	"I am the life of the party.",
+	"I sympathize with others’ feelings.",
+	"I get chores done right away. ",
+	"I have frequent mood swings. ",
+	"I have a vivid imagination. ",
+	"I feel entitled to more of everything.",
+	"I don’t talk a lot.",
+	"I am not interested in other people’s problems.",
+	"I have difficulty understanding abstract ideas.",
+	"I like order.",
+	"I make a mess of things.",
+	"I deserve more things in life.",
+	"I do not have a good imagination.",
+	"I feel others’ emotions.",
+	"I am relaxed most of the time.",
+	"I get upset easily.",
+	"I seldom feel blue.",
+	"I would like to be seen driving around in a very expensive car.",
+	"I keep in the background.",
+	"I am not really interested in others.",
+	"I am not interested in abstract ideas.",
+	"I often forget to put things back in their proper place.",
+	"I talk to a lot of different people at parties.",
+	"I would get a lot of pleasure from owning expensive luxury goods."
+]
+
+
+
+
+
+/* DSM - 5*/
+
+// Instruction:
+// This is a list of things different people might say about themselves. We are interested in how you would describe yourself.  We’d like you to take your time and read each statement carefully, selecting the response that best describes you. 
+// Measured: Very false or often false; Sometimes or somewhat false; Sometimes or somewhat true; Very true or often true
+
+var dsm5 = [
+	"People would describe me as reckless.",
+	"I feel like I act totally on impulse.",
+	"Even though I know better, I can’t stop making rash decisions.",
+	"I often feel like nothing I do really matters.",
+	"Others see me as irresponsible.",
+	"I’m not good at planning ahead. ",
+	"My thoughts often don’t make sense to others. ",
+	"I worry about almost everything. ",
+	"I get emotional easily, often for very little reason.",
+	"I fear being alone in life more than anything else. ",
+	"I get stuck on one way of doing things, even when it’s clear it won’t work. ",
+	"I have seen things that weren’t really there. ",
+	"I steer clear of romantic relationships. ",
+	"I’m not interested in making friends. ",
+	"I get irritated easily by all sorts of things. ",
+	"I don’t like to get too close to people. ",
+	"It’s no big deal if I hurt other peoples’ feelings. ",
+	"I rarely get enthusiastic about anything. ",
+	"I crave attention. ",
+	"I often have to deal with people who are less important than me. ",
+	"I often have thoughts that make sense to me but that other people say are strange. ",
+	"I use people to get what I want. ",
+	"I often “zone out” and then suddenly come to and realize that a lot of time has passed. ",
+	"Things around me often feel unreal, or more real than usual. ",
+	"It is easy for me to take advantage of others."
+];
+/* DERS 
+Instruction:
+This part of the questionnaire takes interest in your emotions and feelings, and how you handle them in general. Please select a response that best represents how often, in general, each statement describes you.
+Measured on a 5-point scale: 
+•	Almost never (0 - 10%) (1)	
+•	Sometimes (11 - 35%) (2)	
+•	About half the time (36 - 65%) (3)	
+•	Most of the time (66 - 90%) (4)	
+•	Almost always (91- 100%) (5)
+
+*/
+var ders_instruction = "This part of the questionnaire takes interest in your emotions and feelings, and how you handle them in general. Please select a response that best represents how often, in general, each statement describes you.";
+var ders = [
+	"I pay attention to how I feel.",
+	"I have no idea how I am feeling.",
+	"I have difficulty making sense out of my feelings.",
+	"I am attentive to my feelings.",
+	"I am confused about how I feel.",
+	"When I am upset, I acknowledge my emotions.",
+	"When I am upset, I become embarrassed for feeling that way.",
+	"When I am upset, I have difficulty getting work done.",
+	"When I am upset, I become out of control.",
+	"When I am upset, I believe that I will remain that way for a long time.",
+	"When I am upset, I believe that I’ll end up feeling very depressed.",
+	"When I am upset, I have difficulty focusing on other things.",
+	"When I am upset, I feel ashamed with myself for feeling that way.",
+	"When I am upset, I feel guilty for feeling that way.",
+	"When I am upset, I have difficulty concentrating.",
+	"When I am upset, I have difficulty controlling my behaviors.",
+	"When I am upset, I believe that wallowing in it is all I can do.",
+	"When I am upset, I lose control over my behaviors.",
+];
+
+/* Short UPPS 
+Instruction
+This part of the questionnaire consists of a number of statements that describe ways in which people act and think. For each statement, please indicate how much you agree or disagree with the statement.
+Measured 4-point scale from Strongly Disagree to Strongly Agree
+*/
+var supps_sf_instruction = "This part of the questionnaire consists of a number of statements that describe ways in which people act and think. For each statement, please indicate how much you agree or disagree with the statement.";
+var supps_sf = [
+	"I generally like to see things through to the end.",
+	"My thinking is usually careful and purposeful.",
+	"When I am in great mood, I tend to get into situations that could cause me problems.",
+	"Unfinished tasks really bother me.",
+	"I like   to stop and think things over before I do them. ",
+	"When I   feel bad, I will often do things I later regret in order to make myself feel   better now.",
+	"Once I get going on something I hate to stop.",
+	"Sometimes   when I feel bad, I can't seem to stop what I am doing even though it is   making me feel worse.",
+	"I quite   enjoy taking risks.",
+	"I tend   to lose control when I am in a great mood.",
+	"I finish what I start.",
+	"I tend to value and follow a rational, “sensible” approach to things",
+	"When I am upset I often act without thinking.",
+	"I welcome new and exciting experiences and sensations, even if they are a little frightening and unconventional",
+	"When I feel rejected, I will often say things that I later regret.",
+	"I would like to learn to fly an airplane.",
+	"Others are shocked or worried about the things I do when I am feeling very excited.",
+	"I would enjoy the sensation of skiing very fast down a high mountain slope.",
+	"I usually think carefully before doing anything.",
+	"I tend to act without thinking when I am really excited."
+];
+
+
+/* BIDR - SF 
+This part of the questionnaire contains statements that different people might use to describe themselves. Please select the answer that best indicates the extent to which you agree or disagree with each statement. Be as honest as possible, but rely on your initial feeling and do not think too much. There are no right or wrong answers; just choose the answer that best describes you.  
+Measured in a 7-point scale from 	Strongly disagree (1) to	Strongly agree (7) 
+*/
+var bidrsf_instruction = "This part of the questionnaire contains statements that different people might use to describe themselves. Please select the answer that best indicates the extent to which you agree or disagree with each statement. Be as honest as possible, but rely on your initial feeling and do not think too much. There are no right or wrong answers; just choose the answer that best describes you.";
+var bidrsf = [
+	"I have not always been honest with myself.",
+	"I always know why I like things. ",
+	"It's hard for me to shut off a disturbing thought.",
+	"I never regret my decisions.",
+	"I sometimes lose out on things because I can't make up my mind soon enough.",
+	"I am a completely rational person.",
+	"I am very confident of my judgments.",
+	"I have sometimes doubted my ability as a lover",
+	"I sometimes tell lies if I have to.",
+	"I never cover up my mistakes.",
+	"There have been occasions when I have taken advantage of someone.",
+	"I sometimes try to get even rather than forgive and forget. ",
+	"I have said something bad about a friend behind his/her back.",
+	"When I hear people talking privately, I avoid listening.",
+	"I never take things that don't belong to me.",
+	"I don't gossip about other people's business."
+];
+
+/* SIAS6
+
+Indicate the degree to which you feel the statement is characteristic or true of you.
+Notes:
+*Reverse scored item  = 8, 10
+
+0=Not at all characteristic or true of me.
+1=Slightly characteristic or true of me.
+2=Moderately characteristic or true of me.
+3=Very characteristic or true of me.
+4=Extremely characteristic or true of me.
+
+*/
+
+var sias6_instruction = "Indicate the degree to which you feel the statement is characteristic or true of you.";
+var sias6 = [
+	"I get nervous if I have to speak with someone in authority (teacher, boss, etc.).",
+	"I have difficulty making eye-contact with others.",
+	"I become tense if I have to talk about myself or my feelings.",
+	"I find difficulty mixing comfortably with the people I work with.",
+	"I tense up if I meet an acquaintance in the street.",
+	"When mixing socially, I am uncomfortable.",
+	"I feel tense if I am alone with just one other person.",
+	"I am at ease meeting people at parties, etc.",
+	"I have difficulty talking with other people.",
+	"I find it easy to think of things to talk about.",
+	"I worry about expressing myself in case I appear awkward.",
+	"I find it difficult to disagree with another’s point of view.",
+	"I have difficulty talking to attractive persons of the opposite sex.",
+	"I find myself worrying that I won’t know what to say in social situations.",
+	"I am nervous mixing with people I don’t know well.",
+	"I feel I’ll say something embarrassing when talking.",
+	"When mixing in a group, I find myself worrying I will be ignored.",
+	"I am tense mixing in a group.",
+	"I am unsure whether to greet someone I know only slightly.",
+];
+
+// the sias_6A includes items: 2, 4, 6, 8, 10, and 13 
+var sias6_sf = [
+	"I have difficulty making eye-contact with others.",
+	"I find difficulty mixing comfortably with the people I work with.",
+	"When mixing socially, I am uncomfortable.",
+	"I am at ease meeting people at parties, etc.", // Reverse
+	"I find it easy to think of things to talk about.", // Reverse
+	"I have difficulty talking to attractive persons of the opposite sex."
+];
+
+
+/* Social phobia scale Social Phobia Scale
+
+Indicate the degree to which you feel the statement is characteristic or true of you.
+
+    0=Not at all characteristic or true of me.
+
+    1=Slightly characteristic or true of me.
+
+    2=Moderately characteristic or true of me.
+
+    3=Very characteristic or true of me.
+
+    4=Extremely characteristic or true of me.
+
+*/
+
+var sps = [
+	"I become anxious if I have to write in front of other people.",
+	"I become self-conscious when using public toilets.",
+	"I can suddenly become aware of my own voice and of others listening to me.",
+	"I get nervous that people are staring at me as I walk down the street.",
+	"I fear I may blush when I am with others.",
+	"I feel self-conscious if I have to enter a room where others are already seated.",
+	"I worry about shaking or trembling when I’m watched by other people.",
+	"I would get tense if I had to sit facing other people on a bus or a train.",
+	"I get panicky that others might see me to be faint, sick or ill.",
+	"I would find it difficult to drink something if in a group of people.",
+	"It would make me feel self-conscious to eat in front of a stranger at a restaurant.",
+	"I am worried people will think my behavior odd.",
+	"I would get tense if I had to carry a tray across a crowded cafeteria.",
+	"I worry I’ll lose control of myself in front of other people.",
+	"I worry I might do something to attract the attention of others.",
+	"When in an elevator I am tense if people look at me.",
+	"I can feel conspicuous standing in a queue.",
+	"I get tense when I speak in front of other people.",
+	"I worry my head will shake or nod in front of others.",
+	"I feel awkward and tense if I know people are watching me."
+];
+
+// the SPS-6A includes Items 4, 7, 8, 15, 16, and 17 from the SPS long form. 
+var sps_sf = [
+	"I get nervous that people are staring at me as I walk down the street.",
+	"I worry about shaking or trembling when I’m watched by other people.",
+	"I would get tense if I had to sit facing other people on a bus or a train.",
+	"I worry I might do something to attract the attention of others.",
+	"When in an elevator I am tense if people look at me.",
+    "I can feel conspicuous standing in a queue."
+];
+
+
+/* NSS-SF
+Noise sensitivity Scale (short form)
+
+Measured on a 6-point scale ranging from “disagree strongly” to “agree strongly.” 
+*/
+var nss_sf_instruction = "Indicate the degree to which you feel the statement is characteristic or true of you.";
+var nss_sf = [
+	"I am sensitive to noise ",
+	"I find it hard to relax in a place that’s noisy",
+	"I get mad at people who make noise that keeps me from falling asleep or getting work done",
+	"I get annoyed when my neighbours are noisy.",
+	"I get used to most noises without much difficulty."
+];
+
+
