274b7ec03f546af9751dc21bd850e65387f58541
Dominique Makowski <dom.mak19@gmail.com>
2022-02-25 12:15:49 +0800

    update

--- video_analysis/analysis_objective.py
+++ video_analysis/analysis_objective.py
@@ -1,18 +1,22 @@
 import glob
 import os
 
+import cv2
 import imageio  # also requires imageio-ffmpeg
 import numpy as np
 import pandas as pd
+import PIL
 import pyllusion as pyl
 import skvideo.utils
 from matplotlib import pyplot as plt
 
+opencv_classifer = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 stimuli_dir = "C:/Dropbox/vimeo/"
 
 data = []  # Initialize empty list for data
 for stim in glob.glob(os.path.join(stimuli_dir, "*.mp4")):
-    print(stim)
+    filename = stim.removeprefix("C:/Dropbox/vimeo\\").removesuffix(".mp4")
+    print(filename)
 
     # Read video
     video = imageio.get_reader(stim, "ffmpeg")
@@ -20,7 +24,9 @@ for stim in glob.glob(os.path.join(stimuli_dir, "*.mp4")):
     # Convert to array (time, height, width, channels)
     videodata = np.array([i for i in video])
 
-    print(plt.imshow(videodata[0]))  # Show first frame
+    # Save first frame
+    # print(plt.imshow(videodata[0]))  # Show first frame
+    PIL.Image.fromarray(videodata[0]).save(stimuli_dir + "frame/" + filename + ".png")
 
     n_frames = video.count_frames()
 
@@ -36,12 +42,13 @@ for stim in glob.glob(os.path.join(stimuli_dir, "*.mp4")):
             "BlueYellow",
             "Colorfulness",
             "Contrast",
-            "Structure",
+            # "Structure",
             "Saturation",
             "Entropy",
             "Brightness",
             "Luminance",
             "Luminance_Perceived",
+            "Faces",
         ]
     }
 
@@ -53,18 +60,37 @@ for stim in glob.glob(os.path.join(stimuli_dir, "*.mp4")):
 
     # Compute per frame
     for i, frame in enumerate(video):
-        if i % 100 != 0:  # Iterate every nth element
+        if i % 300 != 0:  # Iterate every nth element
             continue
         print(np.round(i / n_frames * 100, 2), "%")
-        scores = pyl.analyze_image(frame)
+
+        # Objective characteristics (don't include 'structure' for now)
+        scores = pyl.analyze_image(
+            frame,
+            features=["luminance", "color", "entropy", "colorfulness", "contrast"],
+        )
         for key, value in scores.items():
             out[key][i] = value
+
+        # Count heads
+        grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = opencv_classifer.detectMultiScale(grayImage)
+        out["Faces"][i] = len(faces)
+
+        # # Visualize
+        # import matplotlib.patches as patches
+        # fig, ax = plt.subplots()
+        # ax.imshow(frame)
+        # for (x, y, w, h) in faces:
+        #     ax.add_patch(
+        #         patches.Rectangle((x, y), w, h, linewidth=1, edgecolor="g", facecolor="none")
+        #     )
 
     # Average all elements
     out = {key: [np.nanmean(value)] for (key, value) in out.items()}
 
     # Add info
-    out["File"] = [stim]
+    out["File"] = [filename]
     out["Resolution"] = [str(frame.shape[1]) + "x" + str(frame.shape[0])]
     out["Framerate"] = [n_frames / 30]
     data.append(pd.DataFrame(out))
